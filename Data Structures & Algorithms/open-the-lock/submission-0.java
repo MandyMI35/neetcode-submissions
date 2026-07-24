8ab027620044c54e6de2ae738174f1f6fc22caa7
Mandakini Dalwee <154400810+MandyMI35@users.noreply.github.com>
2026-07-24 13:29:51 +0530

    Add: open-the-lock - submission-0

---
+++ Data Structures & Algorithms/open-the-lock/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int openLock(String[] deadends, String target) {
+        Set<String> s = new HashSet<>(Arrays.asList(deadends));
+        if(s.contains("0000")){return -1;}
+
+        Queue<String> q = new LinkedList<>();
+        q.offer("0000");
+        s.add("0000");
+
+        int turns =0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                String lock = q.poll();
+                if(lock.equals(target)) return turns;
+                for(String next : children(lock)){
+                    if(!s.contains(next)){
+                        q.offer(next);
+                        s.add(next);
+                    }
+                }
+            }
+            turns++;
+        }
+        return -1;
+    }
+    public List<String> children(String lock){
+        List<String> res = new ArrayList<>();
+        for(int i=0;i<4;i++){
+            char[] arr = lock.toCharArray();
+            arr[i]=(char)(((arr[i]-'0'+1)%10)+'0');
+            res.add(new String(arr));
+
+            arr = lock.toCharArray();
+            arr[i]=(char)(((arr[i]-'0'-1+10)%10)+'0');
+            res.add(new String(arr));
+        }
+        return res;
+    }
+}
