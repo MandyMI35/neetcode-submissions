063ce8e9545b92aa5829b692157bd06efbf6173d
Mandakini Dalwee <154400810+MandyMI35@users.noreply.github.com>
2026-08-31 12:39:18 +0530

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.java
@@ -0,0 +1,39 @@
+class TrieNode{
+    TrieNode[] children = new TrieNode[26];
+    boolean endofword=false;
+}
+class WordDictionary {
+    private TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+        for(char c:word.toCharArray()){
+            int i=c-'a';
+            if(curr.children[i]==null) curr.children[i]=new TrieNode();
+            curr = curr.children[i];
+        }
+        curr.endofword=true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word,0,root);
+    }
+    public boolean dfs(String word, int j, TrieNode root){
+        TrieNode cur = root;
+        for(int i=j;i<word.length();i++){
+            char c=word.charAt(i);
+            if(c=='.'){
+                for(TrieNode child : cur.children){
+                    if(child!=null && dfs(word,i+1,child)) return true;
+                } return false;
+            } else {
+                if(cur.children[c-'a']==null) return false;
+                cur = cur.children[c-'a'];
+            }
+        }
+        return cur.endofword;
+    }
+}
