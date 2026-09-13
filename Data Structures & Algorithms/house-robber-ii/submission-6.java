de463d017e6423eecd00cda2096aad31a35d1c18
Mandakini Dalwee <154400810+MandyMI35@users.noreply.github.com>
2026-09-13 09:59:28 +0530

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(1==nums.length) return nums[0];
+        return Math.max(dfs(Arrays.copyOfRange(nums,0,nums.length-1)),dfs(Arrays.copyOfRange(nums,1,nums.length)));
+    }
+    public int dfs(int[] nums){
+        // if(0==nums.length) return 0;
+        if(1==nums.length) return nums[0];
+        int[] dp = new int[nums.length];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[nums.length-1];
+    }
+}
