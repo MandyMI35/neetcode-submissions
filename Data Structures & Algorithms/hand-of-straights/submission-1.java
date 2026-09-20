e566bf03c8a9d3735da1db9e27d12d9b39b3d519
Mandakini Dalwee <154400810+MandyMI35@users.noreply.github.com>
2026-09-20 20:53:35 +0530

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        TreeMap<Integer, Integer> hm = new TreeMap<>();
+        for(int i : hand){
+            hm.put(i, hm.getOrDefault(i,0)+1);
+        }
+        Queue<Integer> q = new LinkedList<>();
+        int lnum=-1, og=0;
+        for(int num : hm.keySet()){
+            if((og>0 && num>lnum+1) || og>hm.get(num)) return false;
+            q.add(hm.get(num)-og);
+            lnum=num;
+            og=hm.get(num);
+            if(q.size()==groupSize) og-=q.poll();
+        }
+        return og==0;
+    }
+}
